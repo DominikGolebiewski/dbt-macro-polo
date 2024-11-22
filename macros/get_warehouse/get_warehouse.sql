8def665cd5fed56778681e6045348cf073cdf3e6
Dominik Golebiewski <dominik.golebiewski@atheon.co.uk>
2024-11-22 15:37:14 +0000

    Fix issue with warehouse name resolution during execution

--- macros/get_warehouse/get_warehouse.sql
+++ macros/get_warehouse/get_warehouse.sql
@@ -1,7 +1,20 @@
 --------------------------------------------------------------------------------
 
 {% macro get_warehouse(incremental_size, fullrefresh_size=none) -%}
-  {{ return(adapter.dispatch('get_warehouse', 'dbt_macro_polo')(incremental_size, fullrefresh_size)) }}
+    {% if not execute %}
+        {# During parsing/compilation, just return the warehouse name #}
+        {% set config = var('warehouse_config', {}) %}
+        {% set validated_config = dbt_macro_polo.validate_warehouse_config(config) %}
+        {% set available_sizes = config.get('warehouse_size', dbt_macro_polo.get_default_warehouse_sizes()) %}
+        {% set sizes = dbt_macro_polo.validate_input_parameters(incremental_size, fullrefresh_size) %}
+        {% set size = sizes.fullrefresh if flags.FULL_REFRESH else sizes.incremental %}
+        {% set validated_size = dbt_macro_polo.validate_warehouse_size(size, available_sizes) %}
+        {% set warehouse_prefix = dbt_macro_polo.resolve_environment(target.name, validated_config) %}
+        {{ return(warehouse_prefix ~ "_" ~ validated_size) }}
+    {% else %}
+        {# During execution, use the adapter dispatch #}
+        {{ return(adapter.dispatch('get_warehouse', 'dbt_macro_polo')(incremental_size, fullrefresh_size)) }}
+    {% endif %}
 {%- endmacro %}
 
 --------------------------------------------------------------------------------
