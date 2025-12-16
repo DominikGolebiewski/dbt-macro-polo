71d5b21bc58d29081972c03510789671ee902019
Dominik Golebiewski <dominik.golebiewski@gocrisp.com>
2025-12-16 22:37:47 +0000

    Testing

--- macros/compute_provisioning/provision_compute.sql
+++ macros/compute_provisioning/provision_compute.sql
@@ -8,15 +8,13 @@
     {% set config = dbt_macro_polo._get_compute_config() %}
 
     {% if not incremental_size %}
-        {% set msg = "incremental_size not defined. Defualting to 'xs'" %}
+        {% set msg = "incremental_size not defined. Defualting to: " ~ config.default_size ~ " for environment: " ~ target.name %}
         {{ dbt_macro_polo.log_event(message=msg, level='WARN', model_id=this, macro_name=macro_name) }}
     {% endif %}
 
     {#-- Normalise inputs, fall back to defaults #}
-    {% set inc_size = (incremental_size | trim | lower | default(config.defaults.incremental)) %}
-    {% set fr_size  = (fullrefresh_size | trim | lower | default(config.defaults.full_refresh)) %}
-
-    {{ dbt_macro_polo.log_error("incremental_size: " ~ inc_size ~ ". fullrefresh_size: " ~ fr_size, macro_name) }}
+    {% set inc_size = incremental_size | trim | lower if incremental_size else config.default_size %}
+    {% set fr_size  = fullrefresh_size | trim | lower if fullrefresh_size else inc_size %}
 
     {% set allowed = config.allowed_sizes | map('lower') | map('trim') | list %}
     {% if inc_size not in allowed %}
@@ -26,7 +24,7 @@
         {{ dbt_macro_polo.log_error("Invalid fullrefresh_size: " ~ fr_size ~ ". Allowed: " ~ allowed | join(', '), macro_name) }}
     {% endif %}
 
-    {% set target_size = fr_size if (fr_size != inc_size and dbt_macro_polo.should_full_refresh()) else inc_size %}
+    {% set target_size = fr_size if dbt_macro_polo.should_full_refresh() else inc_size %}
     {% set warehouse_id = config.prefix ~ '_' ~ target_size %}
 
     {{ dbt_macro_polo.log_event(message="Provisioned warehouse", status=warehouse_id | upper, model_id=this, macro_name=macro_name) }}
