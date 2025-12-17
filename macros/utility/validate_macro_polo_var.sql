1ab705cd426998b695d06404c8f65adba93a0244
Dominik Golebiewski <dominik.golebiewski@gocrisp.com>
2025-12-17 09:16:06 +0000

    Testing

--- macros/utility/validate_macro_polo_var.sql
+++ macros/utility/validate_macro_polo_var.sql
@@ -5,7 +5,7 @@
 {% macro default__validate_macro_polo_var() %}
 
     {% set macro_name = 'validate_macro_polo_var' %}
-    {% set macro_polo = dbt_macro_polo.require(var('macro_polo'), "macro_polo must be defined in project variable. Macro Polo is disabled.", macro_name) %}
+    {% set macro_polo = dbt_macro_polo.require(var('macro_polo', none), "macro_polo must be defined in project variable. Macro Polo is disabled.", macro_name) %}
     {{ return(macro_polo or {}) }}
 
 {% endmacro %}
